1304993a434ffef2574c49d701343e22d87564fa
Kai Piper <kai9987kai@gmail.com>
2019-03-15 22:04:32 +0000

    Update UpperAndLowerCaseTextConverter.py

--- UpperAndLowerCaseTextConverter.py
+++ UpperAndLowerCaseTextConverter.py
@@ -1,27 +1,31 @@
 import tkinter as tk
-
+from tkinter import ttk
 class YourGUI(tk.Tk):
     def __init__(self):
         # inherit tkinter's window methods
         tk.Tk.__init__(self)
+        def close():
+            self.destroy()
 
         tk.Label(self, text="ENTER TEXT:")\
             .grid(row=0, column=0)
-        self.input = tk.Entry(self)
+        self.input = ttk.Entry(self)
         self.input.grid(row=0, column=1)
 
         tk.Label(self, text="CONVERTED TO LOWER CASE:")\
             .grid(row=1, column=0)
-        self.result_lower = tk.Entry(self)
+        self.result_lower = ttk.Entry(self)
         self.result_lower.grid(row=1, column=1)
 
         tk.Label(self, text="CONVERTED TO UPPER CASE:")\
             .grid(row=2, column=0)
-        self.result_upper = tk.Entry(self)
+        self.result_upper = ttk.Entry(self)
         self.result_upper.grid(row=2, column=1)
 
-        tk.Button(self, text="convert", command=self.do_conversion)\
-            .grid(row=3, column=0, columnspan=2)
+        ttk.Button(self, text="Convert", command=self.do_conversion)\
+            .grid(row=3, column=0)
+        ttk.Button(self, text="Close", command=close)\
+            .grid(row=3, column=1)
 
 
 
